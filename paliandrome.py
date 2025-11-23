d2bb35bc26135dddabed44d557a0156f6fa8d9ac
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2025-11-23 09:40:04 +0530

    Update paliandrome.py

--- paliandrome.py
+++ paliandrome.py
@@ -1,1 +1,15 @@
-
+import sys
+if len(sys.argv) >= 4:
+    principle = float(sys.argv[1])
+    time = float(sys.argv[2])
+    rate = float(sys.argv[3])
+    print("User input provided.")
+else:
+    principle = 1000
+    time = 2
+    rate = 2
+si = (principle* time * rate) / 100
+print("The Principal is :", principle)
+print("The Time is :", time)
+print("The Rate is :", rate)
+print("The Simple Interest is :", si)
